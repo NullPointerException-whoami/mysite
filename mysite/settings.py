3a6028dd65f07ebb810d3c69276b3cfc722ee510
NullPointerException-whoami <62454207+NullPointerException-whoami@users.noreply.github.com>
2020-03-21 13:24:08 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,17 +74,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'projectdb',
+#         'USER':'root',
+#         'PASSWORD':'lzh20030114',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projectdb',
-        'USER':'root',
-        'PASSWORD':'lzh20030114',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'NAME': 'JimeiShen$projectdb',
+        'USER': 'JimeiShen',
+        'PASSWORD': 'SJMsjm33',
+        'HOST': 'JimeiShen.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
